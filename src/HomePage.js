a9df77a626059a0f1669638bd315149e67584a3e
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2024-09-18 17:31:32 +0200

    basic css edits for main view

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,17 @@
-import './App.css';
+import './homepage.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="main-page-container">
+      <div className="main-page">
+        <div className="main-page-content">
+            <h1>Unlimited movies, TV shows, and more</h1>
+            <h2>Starts at EUR 9.49. Cancel anytime.</h2>
+            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+            <input type="email" placeholder="Email Address" />
+            <button>Get Started</button>
+          </div>
+        </div>
     </div>
   );
 }
